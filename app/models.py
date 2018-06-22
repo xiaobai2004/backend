54febb7f2ae9de5ae34583cd0cc32b09d519002e
dongyan.xu <dongyanxu@live.cn>
2018-06-22 20:18:50 +0800

    add wenbai tables

--- app/models.py
+++ app/models.py
@@ -10,7 +10,6 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -20,8 +19,51 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+class Scriptures(db.Model):
+    __tablename__ = "wenbai_scriptures"
+    id = db.Column(db.Integer, primary_key=True)
+    scripture_display=db.Column(db.Unicode(16), index=True)
+    scripture_title=db.Column(db.Unicode(128), index=True)
+
+    def __repr__(self):
+        return '<Scripture  %r>' % self.scripture_display
+
+class Chapters(db.Model):
+    __tablename__ = "wenbai_chapters"
+    id = db.Column(db.Integer, primary_key=True)
+    scripture_id=db.Column(db.Integer, db.ForeignKey('wenbai_scriptures.id'))
+    chapter_display=db.Column(db.Unicode(16), index=True)
+    chapter__title=db.Column(db.Unicode(128), index=True)
+
+    def __repr__(self):
+        return '<Chapter  %r>' % self.chapter_display
+
+class Sections(db.Model):
+    __tablename__ = "wenbai_sectons"
+    id = db.Column(db.Integer, primary_key=True)
+    chapter_id=db.Column(db.Integer, db.ForeignKey('wenbai_chapters.id'))
+    section_display=db.Column(db.Unicode(16), index=True)
+    section__title=db.Column(db.Unicode(128), index=True)
+
+    def __repr__(self):
+        return '<Section  %r>' % self.section_display
+
+
+class Sentences(db.Model):
+    __tablename__ = "wenbai_sentences"
+    id = db.Column(db.BigInteger, primary_key=True)
+    scripture_id=db.Column(db.Integer, db.ForeignKey('wenbai_scriptures.id'))
+    chapter_id=db.Column(db.Integer, db.ForeignKey('wenbai_scriptures.id'))
+    section_id=db.Column(db.Integer, db.ForeignKey('wenbai_scriptures.id'))
+    classic_text = db.Column(db.Text )
+    modern_text  = db.Column(db.Text )
+
+    def __repr__(self):
+        return '<Sectence  %r>' % self.id
+    
+
 class TabMaterial(db.Model):
-    __talbename__ = 'tab_materials'
+    __tablename__ = 'tab_materials'
     id = db.Column(db.Integer, primary_key=True)
     chapter_id = db.Column(db.Integer, unique=True, index=True)
     chapter_name = db.Column( db.Unicode, index=True )
---
+++ tools/housekeep.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+log_dir="/data1/gwfy3/excel/"
+while [ true ]
+do
+	sleep 600
+done
+
